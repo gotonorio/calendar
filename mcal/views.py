5f1ae3360a42b47458b2b178d29186cd4c4162eb
Goto Norio <gotonorio@gmail.com>
2020-11-22 19:58:50 +0900

    first commit

---
+++ mcal/views.py
@@ -0,0 +1,14 @@
+from django.views import generic
+from . import mixins
+
+
+class MonthCalendar(mixins.MonthCalendarMixin, generic.TemplateView):
+    """月間カレンダーを表示するビュー"""
+    template_name = 'mcal/month.html'
+    first_weekday = 6
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_month_calendar()
+        context.update(calendar_context)
+        return context
