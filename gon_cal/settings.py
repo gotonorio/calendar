e91d54c0cb824e40e9499f7d3fe23bf632db8f4c
Goto Norio <gotonorio@gmail.com>
2021-04-28 17:29:19 +0900

    バージョン番号を変更

--- gon_cal/settings.py
+++ gon_cal/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 
 # 追加設定
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-VERSION_NO = '2021-04-19'
+VERSION_NO = '2021-04-28'
 # Userモデル
 AUTH_USER_MODEL = 'register.User'
 
