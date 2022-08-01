834dd6eb91a02c521ed78e5cc306063ca95f1755
Goto Norio <gotonorio@gmail.com>
2022-08-01 14:20:22 +0900

    バージョン番号のアップデート

--- gon_cal/settings.py
+++ gon_cal/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # For Django4
 CSRF_TRUSTED_ORIGINS = ['https://*.sophiagardens.org']
-VERSION_NO = '2022-02-11'
+VERSION_NO = '2022-08-01'
 # Userモデル
 AUTH_USER_MODEL = 'register.User'
 
